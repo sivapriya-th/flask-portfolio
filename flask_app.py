70ce66dc0e7471b2f48c6097ceb6cf658f1b9fbd
sivapriya-th <sivapriya_th@yahoo.com>
2026-08-21 11:13:59 +0800

    Initial commit - portfolio Flask app

---
+++ flask_app.py
@@ -0,0 +1,120 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "sqlite:////home/priya2026/mysite/comments.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+app.secret_key = "Priya2026CloudSupportLearning"
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+#Old tutorial user code
+#class User(UserMixin):
+
+#     def __init__(self, username, password_hash):
+#         self.username = username
+#         self.password_hash = password_hash
+
+
+#     def check_password(self, password):
+#         return check_password_hash(self.password_hash, password)
+
+
+#     def get_id(self):
+#         return self.username
+
+# all_users = {
+#     "admin": User("admin", generate_password_hash("secret")),
+#     "bob": User("bob", generate_password_hash("less-secret")),
+#     "caroline": User("caroline", generate_password_hash("completely-secret")),
+# }
+
+class User(UserMixin, db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        return str(self.id)
+
+# Old Tutorial User Loader
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return all_users.get(user_id)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(User, int(user_id))
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=Comment.query.all())
+
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login_page.html", error=False)
+
+    # username = request.form["username"]
+    # if username not in all_users:
+    #     return render_template("login_page.html", error=True)
+    # user = all_users[username]
+
+    username = request.form["username"]
+
+    user = User.query.filter_by(username=username).first()
+
+    if user is None:
+        return render_template("login_page.html", error=True)
+
+    if not user.check_password(request.form["password"]):
+        return render_template("login_page.html", error=True)
+
+    login_user(user)
+    return redirect(url_for('index'))
+
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route("/portfolio/")
+def portfolio():
+    return render_template("portfolio.html")
+
